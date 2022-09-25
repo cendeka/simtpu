4a292f756a53b2ed5ee79d0bc8a44163e4efad56
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2022-09-25 17:32:25 +0700

    update

--- storage/framework/views/161de32cafd55664f88ddb43346b7ffcd3881bfa.php
+++ storage/framework/views/161de32cafd55664f88ddb43346b7ffcd3881bfa.php
@@ -85,11 +85,8 @@
             </div>
         </div>
     </section>
-
-
-    <span id="cam-has-camera"></span>
-    <span id="cam-qr-result">None</span>
-
+    <div id="qr-reader" style="width:500px"></div>
+    <div id="qr-reader-results"></div>
     <!-- ====== Features End ====== -->
     <!-- ====== Blog Start ====== -->
     <section class="ud-blog-grids">
@@ -233,6 +230,38 @@
         </div>
     </section>
     <!-- ====== Contact End ====== -->
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+<script src="https://unpkg.com/html5-qrcode"></script>
+    <script>
+function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete"
+            || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
+
+    docReady(function () {
+        var resultContainer = document.getElementById('qr-reader-results');
+        var lastResult, countResults = 0;
+        function onScanSuccess(decodedText, decodedResult) {
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                // Handle on success condition with the decoded message.
+                console.log(`Scan result ${decodedText}`, decodedResult);
+            }
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "qr-reader", { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
+    });
+    </script>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.main-front', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/ilhamtaufiq/www/simtpuv2/resources/views/pages/front/index.blade.php ENDPATH**/ ?>
