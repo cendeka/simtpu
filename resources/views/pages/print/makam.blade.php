6a664d3a174587827608d2669eef7604b68d7fc2
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2022-10-15 13:05:49 +0700

    fix add data makam (existing)

--- resources/views/pages/print/makam.blade.php
+++ resources/views/pages/print/makam.blade.php
@@ -8,18 +8,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <style>
+            table {
+                border-collapse: collapse;
+            }
+            table td {
+                border: 1px solid black;
+                background-color: #000;
+            }
+            table tr {
+                border: 1px solid black;
+            }
+            .rowspan {
+                border-left-width: 10px;
+            }
         hr {
             margin-top: 1rem;
             margin-bottom: 1rem;
-            border: 0;
-            border-top: 1px solid #000;
+            border: 1px solid black;
+            border-top: 1px solid black;
         }
     </style>
 </head>
 
 <body>
     <div class="row">
-        <div class="col-4" style="border:#000; border-width: 1px;">
+        <div class="col" style="border:#000; border-width: 1px;">
             <img style="height: 64px;"
                 src="https://1.bp.blogspot.com/-j__HA8Uzl-U/YFbp0QmW7GI/AAAAAAAACCs/cE3ZXkgfGoMbWdPPcbySi8en3EJ1sAFRQCNcBGAsYHQ/s2048/Kabupaten%2BCianjur.png"
                 alt="">
